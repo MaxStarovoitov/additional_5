0a64d7d35a5e328e9f25170b66737b395f88f44f
MaxStarovoitov <rackezik@gmail.com>
2018-03-01 17:09:07 +0300

    Solution2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
-
 module.exports = function check(str, bracketsConfig) {
-  var open = [],
-    close = [],
-    buf = [],
-    check = 0;
+  var open = [], close = [], buf = [], check = 0;
 
   bracketsConfig.forEach(function(item, i, bracketsConfig) {
     open[i] = bracketsConfig[i][0];
     close[i] = bracketsConfig[i][1];
   });
 
-  bracketsConfig.forEach(function(item, j, str) {
-    bracketsConfig.forEach(function(item, z, open) {
-
-      if(str[j] == open[z]) { buf.push(z); check ++; }
-      if(str[j] == close[z]) { 
-        if(z == buf.pop()) { check--; }
-        else return false;
-      }
-
-    });
+  for (var i = 0; i < str.length; i++) {
+    for(var j = 0; j < open.length; j++) {
+      if(str[i] == open[j]) { buf.push(j); check++; }
+      if(str[i] == close[j]) { if(j == buf.pop()) { check--; } else { return false; } }
+    }
 
     if(check < 0) return false;
-  });
+  }
 
   if(check == 0) return true;
   return false;
