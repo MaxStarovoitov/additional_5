4534813777789cfc9f186b7ca9b7fe7cf0a3580f
MaxStarovoitov <rackezik@gmail.com>
2018-03-01 17:01:41 +0300

    Solution additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var open = [],
+    close = [],
+    buf = [],
+    check = 0;
+
+  bracketsConfig.forEach(function(item, i, bracketsConfig) {
+    open[i] = bracketsConfig[i][0];
+    close[i] = bracketsConfig[i][1];
+  });
+
+  bracketsConfig.forEach(function(item, j, str) {
+    bracketsConfig.forEach(function(item, z, open) {
+
+      if(str[j] == open[z]) { buf.push(z); check++; }
+      if(str[j] == close[z]) { if(z == buf.pop()) { check--; } else { return false; } }
+
+    });
+
+    if(check < 0) return false;
+  });
+
+  if(check == 0) return true;
+  return false;
+};
